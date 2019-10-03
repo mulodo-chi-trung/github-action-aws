6b12cc1bb593cae89a7c917218898974ae22d90a
chi-trung <chi.trung@bebit.com>
2019-10-04 00:27:07 +0700

    modified main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   # region     = "us-east-2"
-  # access_key = "AKIAXQAG5RWJ7XCCGG6C"
-  # secret_key = "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"
+  # access_key = "AKIAXQAG5RWJ3RAX4DLO"
+  # secret_key = "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"
 }
 
 resource "aws_vpc" "project" {
@@ -13,16 +13,16 @@ resource "aws_vpc" "project" {
   }
 }
 
-# # private subnet #1
-# resource "aws_subnet" "private_app_1" {
-#   cidr_block        = "10.20.0.0/24"
-#   vpc_id            = "${aws_vpc.project.id}"
-#   availability_zone = "us-east-2a"
-#   tags = {
-#     Name       = "Private Subnet"
-#     Visibility = "Private"
-#   }
-# }
+# private subnet #1
+resource "aws_subnet" "private_app_1" {
+  cidr_block        = "10.20.0.0/24"
+  vpc_id            = "${aws_vpc.project.id}"
+  availability_zone = "us-east-2a"
+  tags = {
+    Name       = "Private Subnet"
+    Visibility = "Private"
+  }
+}
 
 # # public subnet #1 for NAT gw
 # resource "aws_subnet" "public_nat_1" {
