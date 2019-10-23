3a2a052a84817b8ed694ab365a51f2d3bc47150a
Mac <mac@chitrung.local>
2019-10-23 11:57:24 +0700

    modified main.tf

--- vpc/main.tf
+++ vpc/main.tf
@@ -16,7 +16,7 @@ resource "aws_vpc" "project" {
 
 # private subnet #1
 resource "aws_subnet" "private_app_1" {
-  cidr_block        = "10.20.1.0/24"
+  cidr_block        = "10.20.0.0/24"
   vpc_id            = "${aws_vpc.project.id}"
   availability_zone = "us-east-2a"
   tags = {
